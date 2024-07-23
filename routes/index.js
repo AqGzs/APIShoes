22303f1eacba00e30a7f1e7aeb3149abfbdf1caf
AQ <nhaqq2003@gmail.com>
2024-07-23 17:20:25 +0700

    Lưu các thay đổi hiện tại trước khi chuyển nhánh

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,16 @@ const router = express.Router();
 
 const shoesRoutes = require('./shoes');
 const authRoutes = require('./auth');
-const favoritesRoutes = require('./favorites');
+const favoriteRoutes = require('./favorites');
 const userRoutes = require('./users');
-<<<<<<< HEAD
 const cartRoutes = require('./cart');
-=======
->>>>>>> ceca45fc7873fd67d74e1c8fb5065f47d502c095
+
 router.use('/shoes', shoesRoutes);
 router.use('/auth', authRoutes);
-router.use('/favorites', favoritesRoutes);
-router.use('/users',userRoutes);
-<<<<<<< HEAD
+router.use('/favorites', favoriteRoutes);
+router.use('/users', userRoutes);
 router.use('/cart', cartRoutes);
-=======
->>>>>>> ceca45fc7873fd67d74e1c8fb5065f47d502c095
 
 module.exports = router;
+
 
